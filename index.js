134bd2066033a2c777fd944f9fc0fc5a9571744c
tkamanda <toelokemwa@gmail.com>
2024-07-22 04:43:56 -0400

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // code your solution here
+function superbowlWin(record) {
+    
+    const winningYear = record.find(game =>game.result === "W");
+        return winningYear ? winningYear.year :undefined;
+    
+}
